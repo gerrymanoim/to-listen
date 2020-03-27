be0d09316b2204a8f47c928b2ca8eaeb43a9756b
newtux <newtux@gmail.com>
2020-03-26 20:57:48 -0400

    give the function a bit of buffer time for token auth

--- functions/process_listens/main.py
+++ functions/process_listens/main.py
@@ -81,7 +81,8 @@ def get_spotify_auth(uid: str) -> Dict:
     auth = db.collection("spotify").document(uid)
     tokens = auth.get().to_dict()
     # handle token refresh if needed
-    if datetime.now(timezone.utc) > tokens["expires_at"]:
+    func_run_time = datetime.now(timezone.utc)-timedelta(seconds=10)
+    if func_run_time > tokens["expires_at"]:
         tokens = refresh_tokens(uid, tokens)
     return tokens
 
