39a844f04e6be523aa49dc380a5c128aeedce29f
Gerry Manoim <gmanoim@quantopian.com>
2020-11-13 00:12:55 -0500

    do some refactors

--- app/main.py
+++ app/main.py
@@ -1,52 +1,61 @@
+from __future__ import annotations
+
+import logging as log
 from base64 import b64encode
 from datetime import datetime, timedelta, timezone
-import logging as log
 from random import randint
-from typing import Any, List, Dict, Optional, Tuple
-
-import requests
-import toml
+from typing import Any, Dict, List, Optional, Tuple
 
 import google.oauth2.id_token
-from google.cloud import firestore
-from google.cloud import secretmanager_v1beta1 as secretmanager
+import requests
+import toml
 from flask import Flask, redirect, render_template, request, url_for
 from google.auth.transport import requests as ga_requests
+from google.cloud import firestore
+from google.cloud import secretmanager_v1beta1 as secretmanager
+
+secrets_client = secretmanager.SecretManagerServiceClient()
+firebase_request_adapter = ga_requests.Request()
+
 
 cfg = toml.load("cfg.toml")
 
-firebase_request_adapter = ga_requests.Request()
-db = firestore.Client()
-secrets_client = secretmanager.SecretManagerServiceClient()
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
 
 
+def render_index(*, auth_url=None, user_data=None, error_message=None):
+    return render_template(
+        "index.html",
+        auth_url=auth_url,
+        user_data=user_data,
+        error_message=error_message,
+    )
+
+
 @app.route("/")
 def main():
-    id_token = request.cookies.get("token")
-    claims, error_message = verify_login(id_token)
-    if claims:
-        auth_url = get_auth_url(claims["sub"])
-    else:
-        auth_url = None
-
-    return render_template(
-        "index.html", auth_url=auth_url, user_data=claims, error_message=error_message
+    try:
+        user = User.from_request_token(request.cookies.get("token"))
+    except ValueError as e:
+        return render_index(error_message=str(e))
+    state = randint(10000, 99999)
+    user["spotify"] = {"state": state}
+    return render_index(
+        auth_url=build_auth_url(state), user_data=user["claims"],
     )
 
 
 @app.route("/auth_callback")
 def auth_callback():
-    id_token = request.cookies.get("token")
-    claims, error_message = verify_login(id_token)
-    maybe_error = request.args.get("error")
-    if maybe_error:
-        return f"Error: {maybe_error}"
-    if error_message:
-        return f"Auth Error: {error_message}"
+    if r_error := request.args.get("error"):
+        return f"Error: {r_error}"
+    try:
+        user = User.from_request_token(request.cookies.get("token"))
+    except ValueError as e:
+        return f"Auth Error: {e}"
 
     code = request.args.get("code")
     client_id, client_secret = get_api_secrets()
@@ -64,132 +73,200 @@ def auth_callback():
         log.error("Error Getting token %s: %s", r, r.text)
         return "Error getting token"
 
-    token = r.json()
-    store_spotify_token(claims["sub"], token)
-    user_profile = get_user_profile(claims["sub"])
-    store_spotify_user_profile(claims["sub"], user_profile)
+    user["spotify"] = r.json()
+    spotify = Spotify.from_user(user)
+    user["spotify_profile"] = spotify.get_profile()
+
     return redirect(url_for("user_info"))
 
 
 @app.route("/user_info")
 def user_info():
-    id_token = request.cookies.get("token")
-    claims, error_message = verify_login(id_token)
-    if error_message:
-        return f"Auth Error: {error_message}"
+    try:
+        user = User.from_request_token(request.cookies.get("token"))
+        spotify = Spotify.from_user(user)
+    except ValueError as e:
+        return f"Auth Error: {e}"
 
-    playlists = get_user_playlists(claims["sub"])
-    saved_playlist = get_spotify_playlist(claims["sub"])
+    playlists = spotify.get_all_playlists()
 
     return render_template(
         "user_info.html",
         playlists=playlists,
-        saved_playlist=saved_playlist,
-        user_data=claims,
+        saved_playlist=user["spotify_playlist"],
+        user_data=user["claims"],
     )
 
 
 @app.route("/save_playlist", methods=["POST"])
 def save_playlist():
-    id_token = request.cookies.get("token")
-    claims, error_message = verify_login(id_token)
-    if error_message:
-        log.error("Error saving playlist")
-        return f"Auth Error: {error_message}"
+    try:
+        user = User.from_request_token(request.cookies.get("token"))
+        spotify = Spotify.from_user(user)
+    except ValueError as e:
+        return f"Auth Error: {e}"
+
     playlist_id = request.form["playlist_id"]
-    playlist = get_user_playlist(claims["sub"], playlist_id)
-    store_spotify_playlist(claims["sub"], playlist)
+    playlist = spotify.get_single_playlist(playlist_id)
+    user["spotify_playlist"] = playlist
     return redirect(url_for("user_info"))
 
 
-def get_user_profile(uid: str) -> Dict[str, Any]:
-    tokens = get_spotify_auth(uid)
-    r = requests.get(
-        cfg["user_url"], headers={"Authorization": "Bearer " + tokens["access_token"]}
-    )
-    if not r.ok:
-        log.error("Error getting user profile %s: %s", r, r.text)
-    return r.json()
+# Supporting code
 
 
-def get_user_playlists(uid: str) -> List[Dict]:
-    tokens = get_spotify_auth(uid)
-    user = get_spotify_user_profile(uid)
-    query_url = cfg["user_playlists_url"].format(user_id=user["id"])
-    out = []
-    while True:
+class TokenExpiredException(Exception):
+    def __init__(self, message, refresh_token):
+        self.message = message
+        self.refresh_token = refresh_token
+
+
+class User:
+
+    valid_keys = ("claims", "spotify", "spotify_playlist", "spotify_profile")
+
+    def __init__(self, uid: str):
+        self.uid = uid
+        self.client = firestore.Client()
+
+    @classmethod
+    def from_request_token(cls, token: str) -> User:
+        """
+        Verify the token we got on the request. If valid, construct a DB object to
+        perform ops. Otherwise raise an error.
+
+        Note this verifies on each page load instead of keeping the token in a Flask
+        session.
+        """
+        try:
+            claims = google.oauth2.id_token.verify_firebase_token(
+                token, firebase_request_adapter
+            )
+        except ValueError:
+            # This is raised by the auth if token is expired or verification fails
+            raise
+        c = cls(claims["sub"])
+        c["claims"] = claims
+        return c
+
+    def _set(self, collection: str, data: dict, merge=True):
+        user_document = self.client.collection(collection).document(self.uid)
+        user_document.set(data, merge=merge)
+
+    def __setitem__(self, k: str, v: dict):
+        if k == "spotify":
+            if "expires_in" in v:
+                expires_at = datetime.now(timezone.utc) + timedelta(
+                    seconds=v["expires_in"]
+                )
+                v = {**v, "expires_at": expires_at}
+            self._set(k, v)
+        elif k == "spotify_profile":
+            self._set(k, v)
+        elif k == "claims":
+            now = datetime.now(timezone.utc)
+            self._set(k, {**v, "last_used": now})
+        elif k == "spotify_playlist":
+            self._set(k, v, merge=False)
+        else:
+            raise KeyError(
+                "Attempt to set invalid key {k}. Must be one of {self.valid_keys}"
+            )
+
+    def _get(self, collection: str) -> Dict[str, Any]:
+        user_document = self.client.collection(collection).document(self.uid)
+        return user_document.get().to_dict()
+
+    def __getitem__(self, k: str) -> Dict[str, Any]:
+        if k == "spotify":
+            auth = self._get("spotify")
+            if datetime.now(timezone.utc) > auth["expires_at"]:
+                raise TokenExpiredException(
+                    f'Auth Token expired at {auth["expires_at"]}',
+                    auth["refresh_token"],
+                )
+            return auth
+        elif k in self.valid_keys:
+            return self._get(k)
+        else:
+            raise KeyError(
+                "Attempt to get invalid key {k}. Must be one of {self.valid_keys}"
+            )
+
+
+class Spotify:
+    def __init__(self, uid: str, access_token: str):
+        self.uid = uid
+        self.access_token = access_token
+
+    @classmethod
+    def from_user(cls, user: User) -> Spotify:
+        try:
+            auth = user["spotify"]
+        except TokenExpiredException as exc:
+            auth = cls._refresh_tokens(exc.refresh_token)
+            user["spotify"] = auth
+        return cls(user.uid, auth["access_token"])
+
+    def _get(self, url: str):
         r = requests.get(
-            query_url, headers={"Authorization": "Bearer " + tokens["access_token"]}
+            url, headers={"Authorization": "Bearer " + self.access_token}
         )
         if not r.ok:
-            log.error("Trouble gettings user playlists %s: %s", r, r.text)
-        playlists = r.json()
-        out.extend([{"id": p["id"], "name": p["name"]} for p in playlists["items"]])
-        if not playlists["next"]:
-            break
-        query_url = playlists["next"]
+            log.error(f"Error in GET {url}: {r.text} - {r}")
+            # raise?
+            return None  # ?
+        return r.json()
 
-    return out
+    def get_profile(self) -> Optional[dict]:
+        return self._get(cfg["profile_url"])
 
+    def get_single_playlist(self, playlist_id: str):
+        url = cfg["single_playlist_url"].format(playlist_id=playlist_id)
+        return self._get(url)
 
-def get_user_playlist(uid: str, playlist_id: str) -> Dict[str, Any]:
-    tokens = get_spotify_auth(uid)
-    query_url = cfg["playlist_url"].format(playlist_id=playlist_id)
-    r = requests.get(
-        query_url, headers={"Authorization": "Bearer " + tokens["access_token"]}
-    )
-    if not r.ok:
-        log.error("Trouble gettings single user playlist %s: %s", r, r.text)
-    return r.json()
-
-
-def refresh_tokens(uid: str, tokens: dict) -> dict:
-    client_id, client_secret = get_api_secrets()
-    client_info = f"{client_id}:{client_secret}".encode("utf-8")
-    b64client = b64encode(client_info)
-    r = requests.post(
-        cfg["token_url"],
-        data={"grant_type": "refresh_token", "refresh_token": tokens["refresh_token"]},
-        headers={"Authorization": "Basic " + b64client.decode("utf-8")},
-    )
-    if not r.ok:
-        log.error("Error refreshing token %s: %s", r, r.text)
-        return "Error getting token"
-
-    new_tokens = r.json()
-    store_spotify_token(uid, new_tokens)
-    return new_tokens
-
-
-def verify_login(id_token: str) -> Tuple[Optional[Dict], Optional[str]]:
-    """Verify token we got on the request"""
-    error_message = None
-    claims = None
-
-    if id_token:
-        try:
-            # Verify the token against the Firebase Auth API. This example
-            # verifies the token on each page load. For improved performance,
-            # some applications may wish to cache results in an encrypted
-            # session store (see for instance
-            # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
-            claims = google.oauth2.id_token.verify_firebase_token(
-                id_token, firebase_request_adapter
+    def get_all_playlists(self) -> List[Dict]:
+        user = self.get_profile()
+        url = cfg["all_playlists_url"].format(user_id=user["id"])
+        out = []
+        while True:
+            playlists = self._get(url)
+            # TODO - handle error
+            out.extend(
+                [
+                    {"id": p["id"], "name": p["name"]}
+                    for p in playlists["items"]
+                ]
             )
-            store_claims(claims)
-        except ValueError as exc:
-            # This will be raised if the token is expired or any other
-            # verification checks fail.
-            error_message = str(exc)
+            if not playlists["next"]:
+                break
+            url = playlists["next"]
 
-    return claims, error_message
+        return out
+
+    @staticmethod
+    def _refresh_tokens(refresh_token: str) -> Dict[str, str]:
+        client_id, client_secret = get_api_secrets()
+        client_info = f"{client_id}:{client_secret}".encode("utf-8")
+        b64client = b64encode(client_info)
+        r = requests.post(
+            cfg["token_url"],
+            data={
+                "grant_type": "refresh_token",
+                "refresh_token": refresh_token,
+            },
+            headers={"Authorization": "Basic " + b64client.decode("utf-8")},
+        )
+        if not r.ok:
+            log.error("Error refreshing token %s: %s", r, r.text)
+            return "Error getting token"
+
+        return r.json()
 
 
-def get_auth_url(uid: str) -> str:
+def build_auth_url(state: int) -> str:
     """Get the spotify auth url we need the user to approve"""
-    state = randint(10000, 99999)
-    store_spotify_auth(uid, {"state": state})
-    client_id, client_secret = get_api_secrets()
+    client_id, _ = get_api_secrets()
     auth_url = (
         "{auth_url_base}"
         "client_id={client_id}&"
@@ -198,70 +275,27 @@ def get_auth_url(uid: str) -> str:
         "state={state}&"
         "scope={scopes_str}"
     ).format(
-        **cfg, state=state, client_id=client_id, scopes_str=" ".join(cfg["scopes"])
+        **cfg,
+        state=state,
+        client_id=client_id,
+        scopes_str=" ".join(cfg["scopes"]),
     )
     return auth_url
 
 
-def store_spotify_auth(uid: str, spotify_data: dict):
-    user = db.collection("spotify").document(uid)
-    user.set(spotify_data, merge=True)
-
-
-def store_spotify_user_profile(uid: str, spotify_data: dict):
-    user = db.collection("spotify_profile").document(uid)
-    user.set(spotify_data, merge=True)
-
-
-def store_spotify_token(uid: str, token: dict):
-    expires_at = datetime.now(timezone.utc) + timedelta(seconds=token["expires_in"])
-    store_spotify_auth(uid, {**token, "expires_at": expires_at})
-
-
-def store_spotify_playlist(uid: str, playlist: dict):
-    user = db.collection("spotify_playlist").document(uid)
-    user.set(playlist)
-
-
-def get_spotify_auth(uid: str) -> dict:
-    user = db.collection("spotify").document(uid)
-    tokens = user.get().to_dict()
-    # handle token refresh if needed
-    if datetime.now(timezone.utc) > tokens["expires_at"]:
-        tokens = refresh_tokens(uid, tokens)
-    return tokens
-
-
-def get_spotify_user_profile(uid: str) -> Dict:
-    user = db.collection("spotify_profile").document(uid)
-    return user.get().to_dict()
-
-
-def get_spotify_playlist(uid: str) -> Dict:
-    playlist = db.collection("spotify_playlist").document(uid)
-    return playlist.get().to_dict()
-
-
-def is_valid_state(uid: str, recieved_state: int):
-    return get_spotify_auth(uid)["state"] == recieved_state
-
-
-def store_claims(claims: dict):
-    user = db.collection("claims").document(claims["sub"])
-    user.set({**claims, **{"last_used": datetime.now(timezone.utc)}}, merge=True)
-
-
 def get_api_secrets() -> Tuple[str, str]:
-    client_id_name = secrets_client.secret_version_path(
-        cfg["project_id"], "spotify_client_id", "latest"
+    response = secrets_client.access_secret_version(
+        request={
+            "name": f'projects/{cfg["project_id"]}/secrets/spotify_client_id/versions/latest'
+        }
     )
-    response = secrets_client.access_secret_version(client_id_name)
     client_id = response.payload.data.decode("UTF-8")
 
-    client_secret_name = secrets_client.secret_version_path(
-        cfg["project_id"], "spotify_client_secret", "latest"
+    response = secrets_client.access_secret_version(
+        request={
+            "name": f'projects/{cfg["project_id"]}/secrets/spotify_client_secret/versions/latest'
+        }
     )
-    response = secrets_client.access_secret_version(client_secret_name)
     client_secret = response.payload.data.decode("UTF-8")
 
     return (client_id, client_secret)
